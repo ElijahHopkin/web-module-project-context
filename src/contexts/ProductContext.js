01d4c5cc63967c3f378eb2dee4c2d86c2f52d858
enhopkin@gmail.com <enhopkin@gmail.com>
2022-04-21 13:19:22 -0600

    mvp complete

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,17 @@
+import  {createContext} from 'react'
+
+
+// const contextObj = {
+
+// }
+
+export const ProductContext= createContext();
+
+
+// export default function ProductProvider (props) {
+//     return(
+//         <ProductContext.Provider value = {contextObj} >
+//             {props.children}
+//         </ProductContext.Provider>
+//     ) 
+// }
